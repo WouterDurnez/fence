932d97b8348bea304b44706305aefe792d27d984
wouter.durnez <wouter.durnez@showpad.com>
2024-08-06 09:59:05 +0200

    🎨 Refactor logging to base

--- fence/models/base.py
+++ fence/models/base.py
@@ -42,15 +42,45 @@ def get_log_tags():
     return _global_log_tags
 
 
+#######################
+# Base class for LLMs #
+#######################
+
+
 class LLM(ABC):
     """
     Base class for LLMs
     """
 
-    model_name = None
-    llm_name = None
+    model_id: str | None = None
+    llm_name: str = None
     inference_type = None
-    source = None
+    source: str = None
+    logging_tags: dict = {}
+
+    def __init__(
+        self,
+        source: str,
+        metric_prefix: str | None = None,
+        extra_tags: dict | None = None,
+        **kwargs,
+    ):
+        """
+        Initialize an LLM
+
+        :param str source: An indicator of where (e.g., which feature) the model is operating from
+        :param str|None metric_prefix: Prefix for the metric names
+        :param dict|None extra_tags: Additional tags to add to the logging tags
+        :param **kwargs: Additional keyword arguments
+        """
+
+        # LLM parameters
+        self.source = source
+
+        # Logging parameters
+        self.metric_prefix = metric_prefix or ""
+        self.logging_tags = get_log_tags() or {}
+        self.logging_tags.update(extra_tags or {})
 
     def __call__(self, prompt: str | Any, **kwargs) -> str:
         return self.invoke(prompt, **kwargs)
@@ -58,3 +88,53 @@ class LLM(ABC):
     @abstractmethod
     def invoke(self, prompt: str | Messages, **kwargs) -> str:
         raise NotImplementedError
+
+
+##########
+# Mixins #
+##########
+
+
+class MessagesMixin:
+    """
+    Mixin for LLMs that take a Messages object as input
+
+    This mixin provides a method to count the number of words in a list of messages
+    """
+
+    @staticmethod
+    def count_words_in_messages(messages: Messages):
+        """
+        Count the number of words in a list of messages. Takes all roles into account. Type must be 'text'
+        :param messages: list of messages
+        :return: word count (int)
+        """
+
+        # Get user and assistant messages
+        user_assistant_messages = messages.messages
+
+        # Get system message
+        system_messages = messages.system
+
+        # Initialize word count
+        word_count = 0
+
+        # Loop through messages
+        for message in user_assistant_messages:
+
+            # Get content
+            content = message.content
+
+            # Content is either a string, or a list of content objects
+            if isinstance(content, str):
+                word_count += len(content.split())
+            elif isinstance(content, list):
+                for content_object in content:
+                    if content_object.type == "text":
+                        word_count += len(content_object.text.split())
+
+        # Add system message to word count
+        if system_messages:
+            word_count += len(system_messages.split())
+
+        return word_count
--- fence/models/claude.py
+++ fence/models/claude.py
@@ -9,7 +9,6 @@ import boto3
 from fence.models.base import (
     LLM,
     get_log_callback,
-    get_log_tags,
     register_log_callback,
     register_log_tags,
 )
@@ -18,10 +17,7 @@ from fence.models.base import (
 class ClaudeBase(LLM):
     """Base class for Claude (gen 1/2) models"""
 
-    model_name: str | None = None
-    llm_name: str | None = None
     inference_type = "bedrock"
-    logging_tags: dict = {}
 
     def __init__(
         self,
@@ -39,52 +35,19 @@ class ClaudeBase(LLM):
         :param **kwargs: Additional keyword arguments
         """
 
-        self.source = source
+        super().__init__(
+            source=source, metric_prefix=metric_prefix, extra_tags=extra_tags
+        )
 
         # LLM parameters
-        self.temperature = kwargs.get("temperature", 0.01)
-        self.max_tokens_to_sample = kwargs.get("max_tokens_to_sample", 2048)
         self.model_kwargs = {
-            "temperature": self.temperature,
-            "max_tokens_to_sample": self.max_tokens_to_sample,
+            "temperature": kwargs.get("temperature", 0.01),
+            "max_tokens_to_sample": kwargs.get("max_tokens_to_sample", 2048),
         }
 
         # AWS parameters
         self.region = kwargs.get("region", "eu-central-1")
         self.client = boto3.client("bedrock-runtime", self.region)
-
-        # Logging parameters
-        self.metric_prefix = metric_prefix or ""
-        self.logging_tags = get_log_tags() or {}
-        self.logging_tags.update(extra_tags or {})
-
-    # def send_token_metrics(
-    #     self, metric_suffix: str, token_count: int, word_count: int
-    # ) -> None:
-    #     """
-    #     Send word and character token metrics to Datadog
-    #     :param metric_suffix: metric suffix to concatenate to the metric name
-    #     :param token_count: token count
-    #     :param word_count: word count
-    #     :return:
-    #     """
-    #
-    #     # Note: keeping this here because other models may include token metrics
-    #     # in their response, so we don't need to calculate them again.
-    #
-    #     metric_prefix = "showpad.data_science.llm.tokens"
-    #     metric_name = f"{metric_prefix}.{metric_suffix}"
-    #     tags = [
-    #         "team:data-science",
-    #         f"llm:{self.llm_name}",
-    #         f"source:{self.source}",
-    #         f"inference_type:{self.inference_type}",
-    #     ]
-    #
-    #     lambda_metric(metric_name=f"{metric_name}_words", value=word_count, tags=tags)
-    #     lambda_metric(
-    #         metric_name=f"{metric_name}_characters", value=token_count, tags=tags
-    #     )
 
     def invoke(self, prompt: str, **kwargs) -> str:
         """
@@ -118,7 +81,6 @@ class ClaudeBase(LLM):
 
         # Log all metrics if a log callback is registered
         if log_callback := get_log_callback():
-
             log_callback(
                 # Add metrics
                 {
@@ -153,7 +115,7 @@ class ClaudeBase(LLM):
         try:
             response = self.client.invoke_model(
                 body=body,
-                modelId=self.model_name,
+                modelId=self.model_id,
                 accept=accept,
                 contentType=contentType,
             )
@@ -176,7 +138,7 @@ class ClaudeInstant(ClaudeBase):
 
         super().__init__(source=source, **kwargs)
 
-        self.model_name = "anthropic.claude-instant-v1"
+        self.model_id = "anthropic.claude-instant-v1"
         self.llm_name = "ClaudeInstant"
 
 
@@ -192,12 +154,11 @@ class ClaudeV2(ClaudeBase):
 
         super().__init__(source=source, **kwargs)
 
-        self.model_name = "anthropic.anthropic.claude-v2"
+        self.model_id = "anthropic.anthropic.claude-v2"
         self.llm_name = "ClaudeV2"
 
 
 if __name__ == "__main__":
-
     # Register logging callback
     register_log_callback(lambda metrics, tags: print(metrics, tags))
 
