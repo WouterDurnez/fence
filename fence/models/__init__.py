12b643f884ec34c05c34151a87bce3077a991418
wouter.durnez <wouter.durnez@showpad.com>
2024-08-19 21:26:42 +0200

    ⚡️ Add Ollama to model init

--- fence/models/__init__.py
+++ fence/models/__init__.py
@@ -1,4 +1,5 @@
-from fence.models.base import LLM  # noqa: F401
-from fence.models.claude import ClaudeBase, ClaudeInstant, ClaudeV2  # noqa: F401
-from fence.models.claude3 import Claude35Sonnet, ClaudeHaiku, ClaudeSonnet  # noqa: F401
-from fence.models.gpt import GPT4o, GPTBase  # noqa: F401
+from .base import LLM  # noqa: F401
+from .claude import ClaudeBase, ClaudeInstant, ClaudeV2  # noqa: F401
+from .claude3 import Claude35Sonnet, ClaudeHaiku, ClaudeSonnet  # noqa: F401
+from .gpt import GPT4o, GPTBase  # noqa: F401
+from .ollama import Llama3_1, Ollama, OllamaBase  # noqa: F401
