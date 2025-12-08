2724e8b6e836a47813b40c826ef29e0aa462163d
Stijn Haus <stijn@interactivehaus.be>
2025-12-08 08:25:42 +0100

    test: Skip non-free tier Gemini tests

--- tests/models/gemini/test_gemini_2_5_flash.py
+++ tests/models/gemini/test_gemini_2_5_flash.py
@@ -8,6 +8,10 @@ from fence.models.gemini.gemini import Gemini2_5_Flash
 # Check if Google Gemini API key is present
 has_gemini_api_key = os.environ.get("GOOGLE_GEMINI_API_KEY") is not None
 
+pytestmark = pytest.mark.skip(
+    reason="Test ignored because model isn't included in Free Tier. Use a Tiered API Key and enable these tests for verification"
+)
+
 
 @pytest.mark.skipif(
     not has_gemini_api_key, reason="Google Gemini API key not found in environment"
